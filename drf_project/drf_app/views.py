838f30e3036910acac4efa12d9da17b5bbb02615
Sergey Gavrilenko <gryroach@gmail.com>
2021-12-24 23:28:14 +0300

    Created .env, Dockerfile, docker-compose.yml Change settings.py

--- drf_project/drf_app/views.py
+++ drf_project/drf_app/views.py
@@ -1,3 +1,5 @@
+import django.utils.datastructures
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -7,7 +9,10 @@ from .serializers import MirrorSerializer
 @api_view(['GET'])
 def mirror_text(request):
     if request.method == 'GET':
-        text = {'text': request.GET['text']}
+        try:
+            text = {'text': request.GET['text']}
+        except django.utils.datastructures.MultiValueDictKeyError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
         serializer = MirrorSerializer(data=text)
         if serializer.is_valid():
             return Response(serializer.data)
--- drf_project/drf_project/settings.py
+++ drf_project/drf_project/settings.py
@@ -11,21 +11,27 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+root = environ.Path(__file__) - 3  # get root of the project
+env = environ.Env()
+environ.Env.read_env(os.path.join(root, '.env'))     # reading .env file
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!c($g3=e+ky1#zorah%2!!$_^9fe2pu^)h)zz=-4!kbnt7#*ey'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
