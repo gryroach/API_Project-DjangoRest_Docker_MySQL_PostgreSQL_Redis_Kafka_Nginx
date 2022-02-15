2ff7dfcebeb6b301b15bccbea140617d201b8bee
gryroach <gryroach@gmail.com>
2022-02-15 18:58:56 +0300

    create CRUD for author (problem for UserID (should rename to post))

--- drf_project/drf_app/urls.py
+++ drf_project/drf_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import mirror_text, SyncPostView, SyncAuthorView
+from .views import mirror_text, SyncPostView, SyncAuthorView, AuthorListView, AuthorDetailView
 
 urlpatterns = [
     path('', mirror_text),
     path('post/sync', SyncPostView.as_view()),
     path('author/sync', SyncAuthorView.as_view()),
+    path('author/', AuthorListView.as_view()),
+    path('author/<int:pk>/', AuthorDetailView.as_view())
 ]
--- drf_project/drf_app/views.py
+++ drf_project/drf_app/views.py
@@ -1,5 +1,7 @@
 import django.utils.datastructures
 import os
+
+from django.http import Http404
 from rest_framework import status, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -46,3 +48,45 @@ class SyncAuthorView(APIView):
             return sync_authors(authors, ex_authors)
         except TypeError:
             return Response("Internal error. Unable to sync authors.", status=400)
+
+
+class AuthorListView(APIView):
+
+    def get(self, request, format=None):
+        snippets = Author.objects.all()
+        serializer = AuthorSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = AuthorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AuthorDetailView(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Author.objects.get(pk=pk)
+        except Author.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        author = self.get_object(pk)
+        serializer = AuthorSerializer(author)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        author = self.get_object(pk)
+        serializer = AuthorSerializer(author, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        author = self.get_object(pk)
+        author.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
