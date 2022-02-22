edb6d757501e3bb4c4dd34abf59a4272cbd9fa2c
gryroach <gryroach@gmail.com>
2022-02-22 15:39:21 +0300

    admin registration model records

--- logs/django_logs/app_logs/admin.py
+++ logs/django_logs/app_logs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import LogRecord
 
-# Register your models here.
+
+@admin.register(LogRecord)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'timestamp', 'type_of_sync')
