c0386e81c04a66cf73674e02eaa9d7aa205343a8
gryroach <gryroach@gmail.com>
2022-02-22 15:28:30 +0300

    create balanced consumer

--- logs/django_logs/app_logs/src/kafka_consumer.py
+++ logs/django_logs/app_logs/src/kafka_consumer.py
@@ -2,3 +2,8 @@ from pykafka import KafkaClient
 
 client = KafkaClient(hosts='kafka:9093')
 topic = client.topics['sync']
+balanced_consumer = topic.get_balanced_consumer(
+    consumer_group='syncgroup',
+    auto_commit_enable=True,
+    zookeeper_connect='zookeeper:2181'
+)
--- logs/django_logs/app_logs/views.py
+++ logs/django_logs/app_logs/views.py
@@ -1,13 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .src.kafka_consumer import client, topic
+from .src.kafka_consumer import client, balanced_consumer
 
 
 @api_view(['GET'])
 def first(request):
     if request.method == 'GET':
-        consumer = topic.get_simple_consumer()
-        for message in consumer:
+        for message in balanced_consumer:
             if message is not None:
                 print(message.offset, str(message.value))
+
         return Response(str(client.topics))
