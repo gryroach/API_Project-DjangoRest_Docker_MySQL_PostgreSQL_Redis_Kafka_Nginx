2cae98ccd4278ab5e22e7289648fc80e153e3b80
gryroach <gryroach@gmail.com>
2022-02-28 15:45:57 +0300

    nginx, gunicorn + static files

--- logs/django_logs/entrypoint.sh
+++ logs/django_logs/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 #set -e # exit if errors happen anywhere
-python manage.py migrate
-
+python manage.py migrate --noinput
+python manage.py collectstatic --no-input --clear
+#gunicorn django_logs.wsgi:application --bind 0.0.0.0:8000
 #uwsgi --socket :8000 --master --enable-threads --module django_logs.wsgi
-gunicorn django_logs.wsgi:application --bind 0.0.0.0:8000
+#gunicorn django_logs.wsgi:application --bind 0.0.0.0:8000
